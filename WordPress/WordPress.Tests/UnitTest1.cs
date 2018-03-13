a448c7fd89a352f55f09de490fc778e28af8c68d
Ivan <attivanm@gmail.com>
2018-03-12 20:05:12 -0400

    adding test case

---
+++ WordPress/WordPress.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordPress.Framework.Browser;
+
+namespace WordPress.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestInitialize]
+        public void Init() {
+            BrowserManager.Instance.Init();
+            BrowserManager.Instance.GoTo();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+
+        [TestCleanup]
+        public void clean() {
+            BrowserManager.Instance.Close();
+        }
+    }
+}
